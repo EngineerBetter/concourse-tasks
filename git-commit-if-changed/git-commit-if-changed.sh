3a4d43c0f0bfdb8f0bb6a8ff9870e0b46790a055
Daniel Jones <daniel.jones@engineerbetter.com>
2020-10-10 20:37:41 +0100

    unparameterised implementation

--- git-commit-if-changed/git-commit-if-changed.sh
+++ git-commit-if-changed/git-commit-if-changed.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
-echo Foo
+set -euxo pipefail
+
+pushd input
+  # Display changed files for debugging
+  git diff-index HEAD --
+
+  # Only commit if the files changed
+  if ! git diff-index --quiet HEAD; then
+    git add -A
+
+    git config --global user.email "systems@engineerbetter.com"
+    git config --global user.name "CI"
+
+    git commit -m "Update dependencies"
+  fi
+popd
+
+git clone ./input ./output

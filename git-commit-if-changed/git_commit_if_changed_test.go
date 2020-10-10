27c1da97e27ace3f54ef24c3223e9677dcb9bbb5
Daniel Jones <daniel.jones@engineerbetter.com>
2020-10-10 20:12:36 +0100

    concatenation instead of templating

--- git-commit-if-changed/git_commit_if_changed_test.go
+++ git-commit-if-changed/git_commit_if_changed_test.go
@@ -1,13 +1,11 @@
 package git_commit_if_changed_test
 
 import (
-	"bytes"
 	"fmt"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
 	"github.com/onsi/gomega/gexec"
-	"html/template"
 	"io/ioutil"
 	"os"
 	"os/exec"
@@ -28,11 +26,11 @@ var _ = Describe("GitCommitIfChanged", func() {
 		Expect(err).NotTo(HaveOccurred())
 		stuff["inputPath"] = inputPath
 
-		bash(`cd {{.inputPath}}
+		bash(`cd `+inputPath+`
 			git init
 			echo foo > file
 			git add -A
-			git commit -m "first commit"`, stuff)
+			git commit -m "first commit"`)
 
 		// Generate temp dir for each output
 		outputPath, err = ioutil.TempDir("", "git-commit-if-changed-output")
@@ -62,8 +60,7 @@ var _ = Describe("GitCommitIfChanged", func() {
 			Expect(err).NotTo(HaveOccurred())
 			configPath := filepath.Join(pwd, "git-commit-if-changed.yml")
 
-			bash(`cd {{.inputPath}}
-			echo bar >> file`, stuff)
+			bash(`cd `+inputPath+` && echo bar >> file`)
 
 			flyArgs := []string{"-t", "eb", "execute", "-c", configPath, "--input=this="+pwd, "--input=input="+inputPath, "--output=output="+outputPath}
 			cmd := exec.Command("fly", flyArgs...)
@@ -73,20 +70,14 @@ var _ = Describe("GitCommitIfChanged", func() {
 			Eventually(session, 2*time.Minute, time.Second).Should(gexec.Exit())
 			Expect(session.ExitCode()).To(BeZero(), message(flyArgs, session))
 
-			after := bash(`cd {{.outputPath}}
-			git status`, stuff)
+			after := bash(`cd `+outputPath+` && git status`)
 			Expect(after.Out).To(gbytes.Say("wibble"))
 		})
 	})
 })
 
-func bash(command string, stuff map[string]interface{}) *gexec.Session {
-	tmpl, err := template.New("command").Parse(command)
-	Expect(err).NotTo(HaveOccurred())
-	var buff bytes.Buffer
-	tmpl.Execute(&buff, stuff)
-	parsed := buff.String()
-	cmd := exec.Command("bash", "-x", "-e", "-u", "-c", parsed)
+func bash(command string) *gexec.Session {
+	cmd := exec.Command("bash", "-x", "-e", "-u", "-c", command)
 	session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 	Expect(err).NotTo(HaveOccurred())
 	Eventually(session, 2*time.Minute, time.Second).Should(gexec.Exit())

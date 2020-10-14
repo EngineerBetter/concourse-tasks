62f422091be534f99d5f9b4ef7882ac8c4f79724
Daniel Jones <daniel.jones@engineerbetter.com>
2020-10-14 08:37:09 +0100

    take args, ie ginkgo -- --specs echo_spec.yml

--- wibble/wibble_suite_test.go
+++ wibble/wibble_suite_test.go
@@ -1,9 +1,13 @@
 package wibble_test
 
 import (
+	"flag"
 	"github.com/EngineerBetter/wibble"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
+	"log"
+	"os"
+	"strings"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
@@ -14,15 +18,34 @@ var specs []*wibble.TaskTestSuite
 var yamlFile []byte
 var setupErr error
 
+var specsArg string
+func init() {
+	flag.StringVar(&specsArg, "specs", "", "Comma-separated list of spec files to execute")
+}
+
 func TestWibble(t *testing.T) {
 	RegisterFailHandler(Fail)
-	loadSpec("echo_spec.yml")
-	loadSpec("file_write_spec.yml")
-	loadSpec("input_spec.yml")
+	specFiles := strings.Split(specsArg, ",")
+
+	if specsArg == "" {
+		log.Fatal("--specs must be provided")
+	}
+
+	for _, specFile := range specFiles {
+		loadSpec(specFile)
+	}
 	RunSpecs(t, "Wibble Suite")
 }
 
 func loadSpec(filename string) {
+	if filename == "" {
+		log.Fatalf("Spec file list (%s) contained empty element", specsArg)
+	}
+
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		log.Fatalf("Spec file '%s' does not exist", filename)
+	}
+
 	yamlFile, setupErr = ioutil.ReadFile(filename)
 	expectErrToNotHaveOccurred(setupErr)
 
@@ -34,6 +57,7 @@ func loadSpec(filename string) {
 }
 
 var _ = BeforeSuite(func() {
+	Expect(specs).ToNot(BeEmpty())
 	// Do validation that the test spec is valid here, where we can use assertions
 	for _, spec := range specs {
 		for _, specCase := range spec.Cases {

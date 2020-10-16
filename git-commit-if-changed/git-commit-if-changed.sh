df3f1ced403ecd27189dae65f12d0385a9490678
Daniel Jones <daniel.jones@engineerbetter.com>
2020-10-16 20:04:13 +0100

    only commit files in list

--- git-commit-if-changed/git-commit-if-changed.sh
+++ git-commit-if-changed/git-commit-if-changed.sh
@@ -2,14 +2,22 @@
 
 set -euxo pipefail
 
+FILES=${FILES:-"--"}
+
+if [ "${FILES}" = "--" ]; then
+  add_flag="-A"
+else
+  add_flag="${FILES}"
+fi
+
 pushd input
   # Display changed files for debugging
   git status
-  git diff-index HEAD --
+  git diff-index HEAD ${FILES}
 
   # Only commit if the files changed
   if ! git diff-index --quiet HEAD; then
-    git add -A
+    git add $add_flag
 
     git config --global user.email "${GIT_AUTHOR_EMAIL}"
     git config --global user.name "${GIT_AUTHOR_NAME}"

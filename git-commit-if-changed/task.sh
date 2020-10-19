dffd654be2a122ffcdf52d6dd385d91d24790f86
Setup <setup@example.com>
2020-10-19 17:22:28 +0100

    Simplify $FILES handling
    We can do this in just plain bash

--- git-commit-if-changed/task.sh
+++ git-commit-if-changed/task.sh
@@ -1,23 +1,15 @@
 #!/usr/bin/env ash
 
 set -euxo pipefail
-
-FILES=${FILES:-"--"}
-
-if [ "${FILES}" = "--" ]; then
-  add_flag="-A"
-else
-  add_flag="${FILES}"
-fi
-
 cd input
+
 # Display changed files for debugging
 git status
 git diff-index HEAD ${FILES}
 
 # Only commit if the files changed
 if ! git diff-index --quiet HEAD; then
-  git add $add_flag
+  git add ${FILES:-"-A"}
 
   git config --global user.email "${GIT_AUTHOR_EMAIL}"
   git config --global user.name "${GIT_AUTHOR_NAME}"

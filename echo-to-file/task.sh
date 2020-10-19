1a21af5d1414566527555e3ee5fd3371c753c8da
Setup <setup@example.com>
2020-10-19 17:38:38 +0100

    Inline task in yml file
    So users don't need to input map this script directory

--- echo-to-file/task.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-: "${FILENAME:?FILENAME env var must be set and not empty}"
-: "${MESSAGE:?MESSAGE env var must be set and not empty}"
-
-cd output
-echo "${MESSAGE}" > "${FILENAME}"
